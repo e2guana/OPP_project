e5004ace256a1ed56d5fcd9d930d186fc30cb4d0
kireal1017 <kireal1017@gmail.com>
2024-10-31 03:45:17 +0900

    reset

--- app/src/main/java/com/example/opp_e2guana/MainActivity.kt
+++ app/src/main/java/com/example/opp_e2guana/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.opp_e2guana
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.page1)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //var buttonState: Boolean = false
+        var text1: TextView = findViewById(R.id.textView)
+        val testButton: Button = findViewById(R.id.JUNG_testbutton)
+
+        testButton.setOnClickListener {
+            text1.text = "버튼 확인"
+        }
+//        if(buttonState) {
+//            text1.text = "텍스트 true"
+//        } else {
+//            text1.text = "텍스트 false"
+//        }
+
     }
 }
